2855eb54318e064a57d6a2d8bbceb6d6ebfe5ea2
Krylov Ivan <91ivan8531@gmail.com>
2017-05-14 13:07:44 +0300

    Задача до пункта №3. Заменил название класса.
    Необходимо для разделения задачи на 2 части

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
@@ -6,24 +6,24 @@ package ru.stqa.pft.sandbox;
 class Start {
   public static void main(String[] args) {
 
-    Point p1 = new Point(2.0, 3.0);
-    Point p2 = new Point(3.0, 4.0);
+    Point1 p1 = new Point1(2.0, 3.0);
+    Point1 p2 = new Point1(3.0, 4.0);
 
-    System.out.println("Расстояние между точками =" + Point.distance(p1, p2));
+    System.out.println("Расстояние между точками = " + Point1.distance(p1, p2));
   }
 }
 
-public class Point {
+public class Point1 {
 
   public double x;
   public double y;
 
-  public Point(double x, double y) {
+  public Point1(double x, double y) {
     this.x = x;
     this.y = y;
   }
 
-  public static double distance(Point p1, Point p2) {
+  public static double distance(Point1 p1, Point1 p2) {
     return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
   }
 }
