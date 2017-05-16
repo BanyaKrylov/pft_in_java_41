4fc1cca9c0fc6ed61fd7d2df98babd11b94cb18b
Krylov Ivan <91ivan8531@gmail.com>
2017-05-16 20:59:36 +0300

    Исправление вызова метода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point2.java
@@ -6,10 +6,21 @@ package ru.stqa.pft.sandbox;
 public class Point2 {
 
   public static void main(String[] args) {
-    distance(2.0, 3.0, 3.0, 4.0);
+    Point1 p1 = new Point1(2.0, 3.0);
+    Point1 p2 = new Point1(3.0, 4.0);
+
+    distance(p1, p2);
   }
 
-  public static void distance(double x1, double x2, double y1, double y2) {
-    System.out.println("Расстояние между точками = " + Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+  public double x;
+  public double y;
+
+  public Point2(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public static void distance(Point1 p1, Point1 p2) {
+    System.out.println("Расстояние между точками = " +  Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)));
   }
 }
