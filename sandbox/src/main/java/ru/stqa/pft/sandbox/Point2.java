8b9e36534b5c92ff02f64cb78ca5a7be8e528803
Krylov Ivan <91ivan8531@gmail.com>
2017-05-17 15:20:37 +0300

    Исправление метода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point2.java
@@ -3,24 +3,28 @@ package ru.stqa.pft.sandbox;
 /**
  * Created by ivan8 on 14.05.2017.
  */
-public class Point2 {
-
+class Start2 {
   public static void main(String[] args) {
-    Point1 p1 = new Point1(2.0, 3.0);
-    Point1 p2 = new Point1(3.0, 4.0);
 
-    distance(p1, p2);
-  }
+      Point2 p1 = new Point2(2.0, 3.0);
+      Point2 p2 = new Point2(3.0, 4.0);
 
-  public double x;
-  public double y;
-
-  public Point2(double x, double y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  public static void distance(Point1 p1, Point1 p2) {
-    System.out.println("Расстояние между точками = " +  Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)));
+      p1.distance(p2);
   }
 }
+    public class Point2 {
+
+
+      public double x;
+      public double y;
+
+      public Point2(double x, double y) {
+        this.x = x;
+        this.y = y;
+      }
+
+      public void distance(Point2 p2) {
+        System.out.println("Расстояние между точками = " + Math.sqrt(Math.pow(p2.x - this.x, 2) + Math.pow(p2.y - this.y, 2)));
+      }
+    }
+
