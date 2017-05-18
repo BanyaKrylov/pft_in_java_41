37907294a5fbeccf3ba8ccf21cf828a8597de89c
Krylov Ivan <91ivan8531@gmail.com>
2017-05-18 22:39:08 +0300

    Задание №4.3
    Параметризация метода заполнения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -18,23 +18,23 @@ public class ContactCreationTests {
     wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
-    login();
+    login("admin", "secret");
   }
 
-  private void login() {
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
   @Test
   public void testContactCreation() {
     initNewContact();
-    fillContact();
+    fillContact(new ContactData("Rubin", "Ivan", "Moscow", "7894561230"));
     submitContact();
     returnHomePage();
   }
@@ -47,19 +47,19 @@ public class ContactCreationTests {
     wd.findElement(By.name("submit")).click();
   }
 
-  private void fillContact() {
+  private void fillContact(ContactData contactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Rubin");
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Ivan");
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastNAme());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Moscow");
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("7894561230");
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
   }
 
   private void initNewContact() {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String lastNAme;
+  private final String address;
+  private final String mobile;
+
+  public ContactData(String firstName, String lastNAme, String address, String mobile) {
+    this.firstName = firstName;
+    this.lastNAme = lastNAme;
+    this.address = address;
+    this.mobile = mobile;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastNAme() {
+    return lastNAme;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+}
