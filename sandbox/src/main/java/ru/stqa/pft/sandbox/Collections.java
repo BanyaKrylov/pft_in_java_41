c32726f1ba3284009427264607d9e5712920f330
Krylov Ivan <91ivan8531@gmail.com>
2017-06-01 20:39:30 +0300

    Коллекции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by ivan8 on 01.06.2017.
+ */
+public class Collections {
+
+  public static void main(String[] args) {
+    String[] langs = {"java", "c#", "phyton", "php"};
+
+    List<String> languages = Arrays.asList("java", "c#", "phyton", "php");
+ 
+    for (String l : languages) {
+      System.out.println("Я хочу выучить " + l);
+    }
+  }
+}
